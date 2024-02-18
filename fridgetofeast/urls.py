bde32df2e188c4cb330c0fa8c28b39e7768688f0
Luis Miguel Torres Villegas <lmtorresv@eafit.edu.co>
2024-02-18 18:51:36 -0500

    Fix typo in Django app name.
    From "frigde..." to "fridge...".

--- fridgetofeast/urls.py
+++ fridgetofeast/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for frigdetofeast project.
+URL configuration for fridgetofeast project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'frigdetofeast.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fridgetofeast.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
