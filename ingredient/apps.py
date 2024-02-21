c8ba3c5165ce422fce6344fef52fd5a25fe5f28a
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2024-02-21 13:51:09 -0500

    Change app name `restaurant` to `ingredient`

--- ingredient/apps.py
+++ ingredient/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RestaurantConfig(AppConfig):
+class IngredientConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'restaurant'
+    name = 'ingredient'
