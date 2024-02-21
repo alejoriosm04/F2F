3409839d657c1daf94d69ea7f645d3d8bd20be19
Luis Miguel Torres Villegas <lmtorresv@eafit.edu.co>
2024-02-21 15:08:27 -0500

    Render the home view with the textbox for the details.

--- fridgetofeast/urls.py
+++ fridgetofeast/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from ingredient import views as ingredientViews
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ingredientViews.index),
 ]
