00ad218278518a721a9af95b6a975368096519db
Luis Miguel Torres Villegas <lmtorresv@eafit.edu.co>
2024-03-02 20:59:00 -0500

    Accept either username or email in login form
    From Chapter 4, section "Building a custom authentication backend" of
    Mele, Antonio. Django 4 by Example. Fourth edition. Birmingham, UK:
    Packt Publishing Ltd., 2022.

--- fridgetofeast/settings.py
+++ fridgetofeast/settings.py
@@ -125,6 +125,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Authentication.
-LOGIN_REDIRECT_URL = ''  # Homepage.
+LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+
+# Custom authentication backends.
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
