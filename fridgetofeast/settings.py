b236b6ac004cd9cadf8c95a7fc5bf1dce14acccf
Luis Miguel Torres Villegas <lmtorresv@eafit.edu.co>
2024-03-03 21:04:12 -0500

    Change background color to the one in the Figma mockup

--- fridgetofeast/settings.py
+++ fridgetofeast/settings.py
@@ -117,7 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = ''
 STATIC_URL = 'static/'
+STATIC_FILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
