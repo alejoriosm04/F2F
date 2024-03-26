1ef6fa344950f91680e82c8545928eafc1880d40
Luis Miguel Torres Villegas <lmtorresv@eafit.edu.co>
2024-03-26 15:14:56 -0500

    Use sync version of OpenAI client instead of async

--- recipe/openAPIadapter.py
+++ recipe/openAPIadapter.py
@@ -1,11 +1,10 @@
-import asyncio
-from openai import AsyncOpenAI
+from openai import OpenAI
 import os
 from django.conf import settings
 
 
 class OpenAIAdapter:
-    client = AsyncOpenAI(api_key=settings.OPENAI_API_KEY)
+    client = OpenAI(api_key=settings.OPENAI_API_KEY)
 
     def generate_response_sync(self,ingredients_string: str, preference):
         instruction = (
@@ -16,18 +15,23 @@ class OpenAIAdapter:
             "As for preferences, I want the recipe to have: {preference}"
         ).format(ingredients=ingredients_string, preference=preference)
 
-        response = asyncio.run(self._generate_response(instruction))
+        response = self._generate_response(instruction)
         cleaned = self.validate_recipe(response)
+
+        if cleaned is not None:
+            image_url = self.generate_image(cleaned)
+            if image_url is not None:
+                cleaned['image_url'] = image_url
 
         return cleaned
 
-    async def _generate_response(self, instruction: str):
+    def _generate_response(self, instruction: str):
         messages = [
             {"role": "system", "content": instruction},
             {"role": "user", "content": "Please create the recipe."}
         ]
 
-        response = await OpenAIAdapter.client.chat.completions.create(
+        response = OpenAIAdapter.client.chat.completions.create(
             model='gpt-3.5-turbo',
             max_tokens=150,
             temperature=0.5,
