d1ccce6d06682a28e6aba27adc3aacfed5c2845a
Luis Miguel Torres Villegas <lmtorresv@eafit.edu.co>
2024-04-17 11:13:33 -0500

    Make clear what is meant by each field of the prompt

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -3,14 +3,16 @@ from .models import Ingredient
 
 class RecipeForm(forms.Form):
   details = forms.CharField(
-    widget=forms.TextInput(attrs={'placeholder': 'I want a recipe that is …'}),
+    widget=forms.TextInput(attrs={'placeholder': 'E.g. "20 minutes to prepare", "for a celebration", etc.'}),
     max_length=100,
     required=False,
+    label="Any specific requests? (preparation time, length, complexity, etc.)",
   )
   preference = forms.CharField(
     widget=forms.TextInput(attrs={'placeholder': 'What kind of cuisine do you like?'}),
     max_length=100,
     required=False,
+    label="What kind of cuisine are you looking for?",
   )
 
 
