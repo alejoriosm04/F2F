alejoriosm04/F2F

commit 8a0358321cfeab26fdc55d96e3b2f841e7e293de

Prevent runtime error by running Matplotlib with a non-interactive backend
https://github.com/alejoriosm04/F2F/issues/9#issuecomment-2010957108

statistic/views.py
@@ -6,6 +6,14 @@ from kitchen.models import Ingredient
 from django.db.models.functions import TruncDay
 from django.db.models import Count
 
+
+# Use a non-interactive Matplotlib backend to prevent a runtime error from
+# starting a Matplotlib GUI outside of main thread.
+# See also: https://stackoverflow.com/q/69924881
+import matplotlib
+matplotlib.use('agg')
+
+
 def overview(request):
     ingredients_per_day = Ingredient.objects.annotate(day=TruncDay('created_at')).values('day').annotate(total=Count('id')).order_by('day')
     days = [ingredient['day'] for ingredient in ingredients_per_day]

commit f5d377c48077a6ed43f0c3885dae9e63c3d03fb2

Fix a potential runtime error with matplotlib
Introduced in commit 8a03583.

statistic/views.py
@@ -12,6 +12,9 @@ from django.db.models import Count
 # Use a non-interactive Matplotlib backend to prevent a runtime error from
 # starting a Matplotlib GUI outside of main thread.
 # See also: https://stackoverflow.com/q/69924881
+import matplotlib
+matplotlib.use('agg')
+
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 